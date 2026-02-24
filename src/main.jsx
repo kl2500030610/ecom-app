c4105d1b4908f62bab4e5d427bd5ca61ae477e67
kl2500030610 <2500030610@kluniversity.in>
2026-02-24 12:24:45 +0530

    updated main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import './index.css'
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App /> 
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
